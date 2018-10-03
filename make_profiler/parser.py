7a2e29b805a9da4f8b31db8fa7dea165fb4559db
Artalus <artalus-mail@yandex.ru>
2018-10-04 00:50:35 +0300

    more tests

--- make_profiler/parser.py
+++ make_profiler/parser.py
@@ -137,10 +137,70 @@ appd.exe: filed.o
 	GCC = gcc -g
 	$(GCC) -o appd.exe filed.o
 	echo "built debug"'''
+        Makefile_medium = Makefile_easy + \
+r'''
+
+ifdef debug
+build: appd.exe
+else
+build: app.exe
+endif'''
+        Makefile_hard = \
+r'''all: build
+
+export X=1
+export Y=2
+
+Z=3
+
+app.exe: file.o
+	gcc -o \
+    app.exe \
+    file.o
+appd.exe: file.o
+	gcc -o app.exe file.o
+
+file.o: file.cpp
+	gcc -c file.cpp -o file.o
+
+ifdef debug
+ifeq ($(debug),1)
+build: appd.exe
+else
+build: app.exe
+endif
+else
+build: appd.exe app.exe
+endif
+
+clean:
+	rm *.o
+	rm *.exe
+#.PHONY : build'''
+
+        def test_comments(self):
+            ss = io.StringIO(\
+            r'''abc def # \
+            zzz''')
+            s = next(ss)
+            # TODO: \ should be ignored since it is in comment
+            self.assertNotEqual(glue_multiline(ss, s), 'abc def # \\')
+            s = next(ss,'')
+            self.assertNotEqual(glue_multiline(ss, s), 'zzz')
+
+            ss = io.StringIO(\
+            r'''abc def '#' \
+            zzz''')
+            s = next(ss)
+            self.assertEqual(glue_multiline(ss, s), "abc def '#' zzz")
 
         tok = io.StringIO()
         def use(self, makestring):
             self.tok = tokenizer(io.StringIO(makestring))
+
+        def skip(self, lines):
+            for _ in range(lines):
+                next(self.tok)
 
         def assertNext(self, type_, line):
             tp, ln = next(self.tok)
@@ -156,6 +216,24 @@ appd.exe: filed.o
             self.assertNext(Tokens.command, 'GCC = gcc -g')
             self.assertNext(Tokens.command, '$(GCC) -o appd.exe filed.o')
             self.assertNext(Tokens.command, 'echo "built debug"')
+
+        def test_tokenizer_medium(self):
+            self.use(self.Makefile_medium)
+            self.skip(6)
+            self.assertNext(Tokens.expression, 'ifdef debug')
+            self.assertNext(Tokens.target, 'build: appd.exe')
+            self.assertNext(Tokens.expression, 'else')
+            self.assertNext(Tokens.target, 'build: app.exe')
+            self.assertNext(Tokens.expression, 'endif')
+
+        def test_tokenizer_hard(self):
+            self.use(self.Makefile_hard)
+            self.skip(2)
+
+            self.assertNext(Tokens.expression, 'export Y=2')
+            self.assertNext(Tokens.expression, 'Z=3')
+            self.assertNext(Tokens.target, 'app.exe: file.o')
+            self.assertNext(Tokens.command, 'gcc -o app.exe file.o')
 
         def test_parse_easy(self):
             tok = io.StringIO(self.Makefile_easy)
